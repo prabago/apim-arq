fee0a6c422419b94c52f89e1309b211e98f0fd12
Pablo Rábago <prabagodelrio@gmail.com>
2020-11-05 20:29:21 +0100

    asdasd

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,6 +6,6 @@ apictl version
 rm -rf $HOME/.wso2apictl
 
 echo 'setting up dev environment'
-apictl add-env -e local-int \
+apictl add-env -e local \
                     --apim https://localhost:9443 \
                     --token https://localhost:8243/token
