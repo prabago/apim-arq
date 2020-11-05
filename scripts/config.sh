66688e6e3b7e7f2d9a9431cf35669d786d636a87
Pablo Rábago <prabagodelrio@gmail.com>
2020-11-05 16:54:43 +0100

    add new env

--- scripts/config.sh
+++ scripts/config.sh
@@ -9,3 +9,6 @@ echo 'setting up dev environment'
 apictl add-env -e local \
                     --apim https://localhost:9443 \
                     --token https://localhost:8243/token
+apictl add-env -e int \
+                    --apim https://localhost:9445 \
+                    --token https://localhost:8243/token
