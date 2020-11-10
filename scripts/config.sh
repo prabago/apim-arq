58eedda2b4e18904811001b5f7469e780d479e4c
Pablo Rábago <prabagodelrio@gmail.com>
2020-11-10 23:37:41 +0100

    asdasd

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,3 +7,6 @@ echo 'setting up dev environment'
 apictl add-env -e local \
                     --apim https://localhost:9443 \
                     --token https://localhost:8243/token
+apictl add-env -e des \
+                    --apim https://am18co7apibus.localdomain:9443 \
+                    --token https://am18co7apibus.localdomain:8243/token
